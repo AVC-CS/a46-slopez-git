c594214d5e165429625d335c0adc5671c71d64a8
Kyu Lee <kwooler@gmail.com>
2026-03-17 11:34:27 -0700

    Simplify starter: one TODO for if structure, keep output statement

--- main.cpp
+++ main.cpp
@@ -9,23 +9,16 @@ int main()
     double factor = 0.1;
     double scholarship = 10000.0;
 
-    // TODO
+    // Use the following input format: <status> <gender> <major>
+    // student female cs ; or student male math ; or nonstudent male cs ; etc.
+    cin >> status >> gender >> major;
 
-    // TODO
-        // TODO
-            // TODO
-                // TODO
-                // TODO
-            // TODO
-                // TODO
-                // TODO
-        // TODO
-            // TODO
+    // TODO: Determine factor based on status, gender, and major using if statements
 
-    // TODO
+    double finalScholarship = scholarship * factor;
 
     cout << setprecision(2) << fixed;
-    // TODO
+    cout << "Decided Factor: " << factor << ", Final Scholarship Amount: " << finalScholarship << endl;
 
     return 0;
 }
